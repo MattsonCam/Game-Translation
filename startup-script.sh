8a9c6f36082a979c48d8295b85fcec210cc1199d
MattsonCam <MattsonCam>
2023-12-12 21:15:34 -0700

    Modified vm startup to run docker containers in the background

--- startup-script.sh
+++ startup-script.sh
@@ -32,9 +32,9 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Start the docker container on port 5000
-sudo docker run -p 5000:5000 --name my-flask --rm cama1171/project-rest-img:v1
-sudo docker run -p 3000:3000 --name my-react --rm cama1171/react-img:v1
-sudo docker run -p 5009:5009 --name my-llm --rm cama1171/project-llm-img:v1
+sudo docker run -d -p 5000:5000 --name my-flask --rm cama1171/project-rest-img:v1
+sudo docker run -d -p 3000:3000 --name my-react --rm cama1171/react-img:v1
+sudo docker run -d -p 5009:5009 --name my-llm --rm cama1171/project-llm-img:v1
 #git clone https://github.com/cu-csci-4253-datacenter/flask-tutorial
 #cd flask-tutorial
 #sudo python3 setup.py install
