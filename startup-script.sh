d7d6aa57b0687e70069a4b5749510d54c6f841be
MattsonCam <MattsonCam>
2023-12-12 20:57:17 -0700

    Added code to run react docker at vm startup

--- startup-script.sh
+++ startup-script.sh
@@ -33,6 +33,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 # Start the docker container on port 5000
 sudo docker run -p 5000:5000 --name my-flask --rm cama1171/project-rest-img:v1
+sudo docker run -p 3000:3000 --name my-react --rm cama1171/react-img:v1
 sudo docker run -p 5009:5009 --name my-llm --rm cama1171/project-llm-img:v1
 #git clone https://github.com/cu-csci-4253-datacenter/flask-tutorial
 #cd flask-tutorial
